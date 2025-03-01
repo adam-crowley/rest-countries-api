a50ebd8d4112cd4520933e0dba637aedec14b9ca
Adam Crowley <adamcrowley@gmail.com>
2025-03-02 12:20:12 +1300

    Add default style

---
+++ src/components/Footer.tsx
@@ -0,0 +1,15 @@
+function Footer() {
+  return (
+    <footer className="footer">
+      <p>
+        Challenge by{' '}
+        <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+          Frontend Mentor
+        </a>
+        . Coded by <a href="https://github.com/adam-crowley">Adam Crowley</a>.
+      </p>
+    </footer>
+  )
+}
+
+export default Footer
